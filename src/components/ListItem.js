42f8c122cb9a55e18bfd5338bf8397ae9264b685
imamsutono <imamssutono@gmail.com>
2018-01-21 14:51:21 +0700

    Separates form and list

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import { Actions } from 'react-native-router-flux';
 import { CardSection } from './common';
 
 class ListItem extends Component {
+  onRowPress() {
+    Actions.employeeEdit({ employee: this.props.employee });
+  }
+
   render() {
     const { name } = this.props.employee;
 
     return (
-      <CardSection>
-        <Text style={styles.title}>{name}</Text>
-      </CardSection>
+      <TouchableWithoutFeedback onPress={this.onRowPress.bind(this)}>
+        <View>
+          <CardSection>
+            <Text style={styles.title}>{name}</Text>
+          </CardSection>
+        </View>
+      </TouchableWithoutFeedback>
     );
   }
 }
