7684aaa7e757d1108538e27a5a68e137f8e31c15
imamsutono <imamssutono@gmail.com>
2018-01-25 13:41:36 +0700

    Done delete employee

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -3,10 +3,12 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { text } from 'react-native-communications';
 import EmployeeForm from './EmployeeForm';
-import { employeeUpdate, employeeSave } from './actions';
-import { Card, CardSection, Button } from './common';
+import { employeeUpdate, employeeSave, employeeDelete } from './actions';
+import { Card, CardSection, Button, Confirm } from './common';
 
 class EmployeeEdit extends Component {
+  state = { showModal: false };
+
   componentWillMount() {
     _.each(this.props.employee, (value, prop) => {
       this.props.employeeUpdate({ prop, value });
@@ -25,6 +27,16 @@ class EmployeeEdit extends Component {
     text(phone, `Your upcoming shift is on ${shift}`);
   }
 
+  onAccept() {
+    const { uid } = this.props.employee;
+
+    this.props.employeeDelete({ uid });
+  }
+
+  onDecline() {
+    this.setState({ showModal: false });
+  }
+
   render() {
     return (
       <Card>
@@ -37,6 +49,20 @@ class EmployeeEdit extends Component {
         <CardSection>
           <Button onPress={this.onTextPress.bind(this)}>Text Schedule</Button>
         </CardSection>
+
+        <CardSection>
+          <Button onPress={() => this.setState({ showModal: true })}>
+            Fire Employee
+          </Button>
+        </CardSection>
+
+        <Confirm
+          visible={this.state.showModal}
+          onAccept={this.onAccept.bind(this)}
+          onDecline={this.onDecline.bind(this)}
+        >
+          Are you sure want to delete this?
+        </Confirm>
       </Card>
     );
   }
@@ -49,5 +75,5 @@ const mapStateToProps = state => {
 };
 
 export default connect(mapStateToProps, {
-  employeeUpdate, employeeSave
+  employeeUpdate, employeeSave, employeeDelete
 })(EmployeeEdit);
--- src/components/actions/EmployeeActions.js
+++ src/components/actions/EmployeeActions.js
@@ -50,3 +50,13 @@ export const employeeSave = ({ name, phone, shift, uid }) => {
       });
   };
 };
+
+export const employeeDelete = ({ uid }) => {
+  const { currentUser } = firebase.auth();
+
+  return () => {
+    firebase.database().ref(`/users/${currentUser.uid}/employees/${uid}`)
+      .remove()
+      .then(() => Actions.pop());
+  };
+}
